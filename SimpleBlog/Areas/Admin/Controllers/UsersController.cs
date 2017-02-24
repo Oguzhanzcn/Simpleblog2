106de54fc4951606da712ae4385eeb789e63580b
OGUZHAN\oğuzhan <oguzhanozcan39@hotmail.com>
2017-02-24 20:22:26 +0300

    Added Admin Area

---
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Admin Area Users Controller Index Action");
+        }
+        public ActionResult List()
+        {
+            return Content("List Deneme");
+        }
+        public ActionResult Edit(int id = 0)
+        {
+
+            return Content("Edit List " + id);
+            //deneme 
+        }
+    }
+}
